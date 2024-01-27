d8adad4624abf3988061f9ef8275caca88b21d94
humam-hossain <humam.hossain.e@gmail.com>
2024-01-27 17:18:05 +0600

    [UPDATE] basic voronoi diagram algorithm implemented

--- src/main.c
+++ src/main.c
@@ -4,15 +4,49 @@
 #include <errno.h>
 #include <assert.h>
 #include <string.h>
+#include <time.h>
 
 #define WIDTH 800
 #define HEIGHT 600
-#define COLOR_RED 0xFF0000FF
+#define SEEDS_COUNT 20
+#define SEED_MARKER_RADIUS 5
+
+#define COLOR_WHITE 0xFFFFFFFF
+#define COLOR_BLACK 0xFF000000
+#define COLOR_RED   0xFF0000FF
+#define COLOR_GREEN 0xFF00FF00
+#define COLOR_BLUE  0xFFFF0000
+#define BACKGROUND_COLOR 0xFF181818
+#define SEED_MARKER_COLOR COLOR_BLACK
+
+#define GRUVBOX_BRIGHT_RED     0xFF3449FB  
+#define GRUVBOX_BRIGHT_GREEN   0xFF26BBB8  
+#define GRUVBOX_BRIGHT_YELLOW  0xFF2FBDFA  
+#define GRUVBOX_BRIGHT_BLUE    0xFF98A583  
+#define GRUVBOX_BRIGHT_PURPLE  0xFF9B86D3  
+#define GRUVBOX_BRIGHT_AQUA    0xFF7CC08E  
+#define GRUVBOX_BRIGHT_ORANGE  0xFF1980FE
+
 #define OUTPUT_FILE_PATH "output.ppm"
 
 typedef uint32_t Color32;
+typedef struct {
+    int x, y;
+} Point;
 
 static Color32 image[HEIGHT][WIDTH];
+static Point seeds[SEEDS_COUNT];
+static Color32 palette[] = {
+    GRUVBOX_BRIGHT_RED,
+    GRUVBOX_BRIGHT_GREEN,
+    GRUVBOX_BRIGHT_AQUA,
+    GRUVBOX_BRIGHT_YELLOW,
+    GRUVBOX_BRIGHT_BLUE,
+    GRUVBOX_BRIGHT_PURPLE,
+    GRUVBOX_BRIGHT_ORANGE,
+};
+
+int palette_count = sizeof(palette)/sizeof(palette[0]);
 
 void fill_image(Color32 color)
 {
@@ -37,7 +71,7 @@ void save_image_as_ppm(const char *file_path)
         for(size_t x = 0; x < WIDTH; ++x){
             // 0xAABBGGRR
             uint32_t pixel = image[y][x];
-            uint32_t bytes[3] = {
+            uint8_t bytes[3] = {
                 (pixel&0x0000FF)>>8*0,
                 (pixel&0x00FF00)>>8*1,
                 (pixel&0xFF0000)>>8*2,
@@ -51,9 +85,70 @@ void save_image_as_ppm(const char *file_path)
     assert(ret == 0);
 }
 
+void generate_random_seeds()
+{
+    for(size_t i = 0; i < SEEDS_COUNT; ++i){
+        seeds[i].x = rand() % WIDTH;
+        seeds[i].y = rand() % HEIGHT;
+    }
+}
+
+int sqr_dist(int x1, int y1, int x2, int y2){
+    int dx = x1 - x2;
+    int dy = y1 - y2;
+
+    return dx*dx + dy*dy;
+}
+
+void fill_circle(int cx, int cy, int radius, Color32 color){
+    int x0 = cx - radius;
+    int y0 = cy - radius;
+    int x1 = cx + radius;
+    int y1 = cy + radius;
+
+    for(int y = y0; y <= y1; ++y){
+        if(0 <= y && y < HEIGHT){
+            for(int x = x0; x <= x1; ++x){
+                if(0 <= x && x < WIDTH){
+                    if(sqr_dist(cx, cy, x, y) <= radius*radius){
+                        image[y][x] = color;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void render_seed_markers()
+{
+    for(size_t i = 0; i < SEEDS_COUNT; ++i){
+        fill_circle(seeds[i].x, seeds[i].y, SEED_MARKER_RADIUS, SEED_MARKER_COLOR);
+    }
+}
+
+void render_voronoi()
+{
+    for(int y = 0; y < HEIGHT; ++y){
+        for(int x = 0; x < WIDTH; ++x){
+            int j = 0;
+            for(int i = 0; i < SEEDS_COUNT; ++i){
+                if(sqr_dist(seeds[i].x, seeds[i].y, x, y) < sqr_dist(seeds[j].x, seeds[j].y, x, y)){
+                    j = i;
+                }    
+            }
+
+            image[y][x] = palette[j % palette_count];
+        }
+    }
+}
+
 int main()
 {
-    fill_image(COLOR_RED);
+    srand(time(0));
+    fill_image(BACKGROUND_COLOR);
+    generate_random_seeds();
+    render_voronoi();
+    render_seed_markers();
     save_image_as_ppm(OUTPUT_FILE_PATH);
 
     return 0;
