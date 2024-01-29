50d2c778f1cbf94e69dfcbea3c5c5687b6731307
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2024-01-30 03:41:40 +0600

    [SETUP] opengl

--- src/main_opengl.c
+++ src/main_opengl.c
@@ -1,8 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define GLFW_INCLUDE_GLEXT
+#include <GLFW/glfw3.h>
+
+#include "glextloader.c"
+
+#define DEFAULT_SCREEN_WIDTH 1600
+#define DEFAULT_SCREEN_HEIGHT 900
+
+void MessageCallback(GLenum source,
+                     GLenum type,
+                     GLuint id,
+                     GLenum severity,
+                     GLsizei length,
+                     const GLchar* message,
+                     const void* userParam)
 {
-    printf("Hello, World!\n");
+    (void) source;
+    (void) id;
+    (void) length;
+    (void) userParam;
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+            type, severity, message);
+}
+
+int main(void)
+{
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not initialize GLFW\n");
+        exit(1);
+    }
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+
+    GLFWwindow * const window = glfwCreateWindow(
+                                    DEFAULT_SCREEN_WIDTH,
+                                    DEFAULT_SCREEN_HEIGHT,
+                                    "OpenGL Template",
+                                    NULL,
+                                    NULL);
+    if (window == NULL) {
+        fprintf(stderr, "ERROR: could not create a window.\n");
+        glfwTerminate();
+        exit(1);
+    }
+
+    int gl_ver_major = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MAJOR);
+    int gl_ver_minor = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MINOR);
+    printf("OpenGL %d.%d\n", gl_ver_major, gl_ver_minor);
+
+    glfwMakeContextCurrent(window);
+
+    load_gl_extensions();
+
+    if (glDrawArraysInstanced == NULL) {
+        fprintf(stderr, "Support for EXT_draw_instanced is required!\n");
+        exit(1);
+    }
+
+    if (glDebugMessageCallback != NULL) {
+        glEnable(GL_DEBUG_OUTPUT);
+        glDebugMessageCallback(MessageCallback, 0);
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    double prev_time = 0.0;
+    double delta_time = 0.0f;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        double cur_time = glfwGetTime();
+        delta_time = cur_time - prev_time;
+        prev_time = cur_time;
+    }
 
     return 0;
 }
